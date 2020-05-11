f5bebeb457d46adfcfddd5f071858afc7240905a
Marin Ueda <marin96nym@gmail.com>
2020-05-11 11:02:53 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by 植田真梨 on 2020/05/11.
+//  Copyright © 2020 Ueda Maririn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
